e8f6cd355ed02881190a18a250d4dab5f1299ddc
HarmoniTech <takasi75926@gmail.com>
2024-05-09 09:33:54 -0700

    Feat: first commit for skeleton

---
+++ frontend/src/portfolio.js
@@ -0,0 +1,73 @@
+
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://www.catloverdev.com/',
+  title: 'Conrad Takashi',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Conrad Takashi',
+  role: 'AI, Blockchain, FullStack, Design',
+  description:
+    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+  resume: 'pdf/ConradTakashiResume.pdf',
+  social: {
+    github: 'https://github.com/HarmoniTech',
+    // linkedin: 'https://linkedin.com',
+    whatsapp: 'https://wa.me/13137423660',
+    discord: 'https://discord.com/users/1170041065093484605',
+    telegram: 'https://t.me/HarmoniTechTeam',
+    twitter: 'https://twitter.com/harmonitechteam'
+  },
+}
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'ML',
+  'AI',
+  'Smart Contact',
+  'Web3',
+  'Btc ordinals',
+  'Btc runes',
+  'Sei',
+  'Python',
+  'Rust',
+  'Go',
+  'Solidity',
+  'Django',
+  'Flask',
+  'Streamlit',
+  'MySQL',
+  'MongoDB',
+  'PostgreSQL',
+  'firebase',
+  'Pinecone',
+  'Weaviate',
+  'ChromaDB',
+  'JavaScript',
+  'TypeScript',
+  'Node.js',
+  'Express.js',
+  'React',
+  'Next.js',
+  'Redux',
+  'Vue.js',
+  'Nuxt.js',
+  'HTML',
+  'CSS',
+  'TailwindCSS',
+  'SASS',
+  'Git',
+  'Docker',
+  'Heroku',
+  'AWS'
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'harmonitechteam@gmail.com',
+}
+
+export { header, about, skills, contact }
